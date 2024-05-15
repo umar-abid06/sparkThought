d91937299d431fb84d39ab4744b953e48be9e844
Muhammad Umar Abid <mumarabid656@gmail.com>
2024-05-16 00:33:32 +0200

    Auth Service

---
+++ src/appwrite/auth.service.js
@@ -0,0 +1,58 @@
+import { Account, Client, ID } from "appwrite";
+import config from "../config/config";
+
+export class AuthService {
+  client = new Client();
+  account;
+
+  constructor() {
+    this.client
+      .setEndpoint(config.appwriteUrl)
+      .setProject(config.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount(email, password) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password
+      );
+      if (userAccount) {
+        // Call another method if there is account then we call Login
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login(email, password) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser error :: ", error);
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      return await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Appwrite service :: getCurrentUser error :: ", error);
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
