656bf08d223b84c603dec669711552b856390562
Muhammad Umar Abid <mumarabid656@gmail.com>
2024-05-19 23:41:17 +0200

    UI Components, Custom Components with ForwardRef

---
+++ src/components/header/LogoutBtn.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import authService from "../../appwrite/auth.service";
+import { logout } from "../../features/auth/authSlice";
+import { useDispatch } from "react-redux";
+
+const LogoutBtn = () => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    authService.logout().then(() => {
+      dispatch(logout());
+    });
+  };
+  return (
+    <button
+      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
+};
+
+export default LogoutBtn;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,21 @@
 import Header from "./header/Header";
 import Footer from "./footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import Select from "./Select";
+import PostCard from "./PostCard";
 
-export { Header, Footer };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  PostCard,
+  Button,
+  Select,
+  Input,
+};
